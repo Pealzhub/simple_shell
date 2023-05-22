7968f594a4cfdbe9d2960d341c26fab1a9c653b2
Nyangimo <rebeccahnyangige9@gmail.com>
2023-05-22 01:04:29 -0700

    parser

--- tokenizer.c
+++ tokenizer.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 /**
-* **strtow - splits a string into words. Repeat delimiters are ignored
+* **strtow – to split a string into words. Repeat delimiters are ignored
 * @str: the input string
 * @d: the delimeter string
 * Return: a pointer to an array of strings, or NULL on failure
@@ -18,7 +18,7 @@ if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
 numwords++;
 if (numwords == 0)
 return (NULL);
-s = malloc((1 + numwords) * sizeof(char *));
+s = malloc((1 + numwords) *sizeof(char *));
 if (!s)
 return (NULL);
 for (i = 0, j = 0; j < numwords; j++)
@@ -61,7 +61,7 @@ if ((str[i] != d && str[i + 1] == d) ||
 numwords++;
 if (numwords == 0)
 return (NULL);
-s = malloc((1 + numwords) * sizeof(char *));
+s = malloc((1 + numwords) *sizeof(char *));
 if (!s)
 return (NULL);
 for (i = 0, j = 0; j < numwords; j++)
@@ -85,149 +85,5 @@ s[j][m] = 0;
 }
 s[j] = NULL;
 return (s);
-}
-========================
-vars.c
-#include "shell.h"
-/**
-* is_chain - test if current char in buffer is a chain delimeter
-* @info: the parameter struct
-* @buf: the char buffer
-* @p: address of current position in buf
-*
-* Return: 1 if chain delimeter, 0 otherwise
-*/
-int is_chain(info_t *info, char *buf, size_t *p)
-{
-size_t j = *p;
-if (buf[j] == '|' && buf[j + 1] == '|')
-{
-buf[j] = 0;
-j++;
-info->cmd_buf_type = CMD_OR;
-}
-else if (buf[j] == '&' && buf[j + 1] == '&')
-{
-buf[j] = 0;
-j++;
-info->cmd_buf_type = CMD_AND;
-}
-else if (buf[j] == ';') /* found end of this command */
-{
-buf[j] = 0; /* replace semicolon with null */
-info->cmd_buf_type = CMD_CHAIN;
-}
-else
-return (0);
-*p = j;
-return (1);
-}
-/**
-* check_chain - checks we should continue chaining based on last status
-* @info: the parameter struct
-* @buf: the char buffer
-* @p: address of current position in buf
-* @i: starting position in buf
-* @len: length of buf
-*
-* Return: Void
-*/
-void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
-{
-size_t j = *p;
-if (info->cmd_buf_type == CMD_AND)
-{
-if (info->status)
-{
-buf[i] = 0;
-j = len;
-}
-}
-if (info->cmd_buf_type == CMD_OR)
-{
-if (!info->status)
-{
-buf[i] = 0;
-j = len;
-}
-}
-*p = j;
-}
-/**
-* replace_alias - replaces an aliases in the tokenized string
-* @info: the parameter struct
-*
-* Return: 1 if replaced, 0 otherwise
-*/
-int replace_alias(info_t *info)
-{
-int i;
-list_t *node;
-char *p;
-for (i = 0; i < 10; i++)
-{
-node = node_starts_with(info->alias, info->argv[0], '=');
-if (!node)
-return (0);
-free(info->argv[0]);
-p = _strchr(node->str, '=');
-if (!p)
-return (0);
-p = _strdup(p + 1);
-if (!p)
-return (0);
-info->argv[0] = p;
-}
-return (1);
-}
-/**
-* replace_vars - replaces vars in the tokenized string
-* @info: the parameter struct
-*
-* Return: 1 if replaced, 0 otherwise
-*/
-int replace_vars(info_t *info)
-{
-int i = 0;
-list_t *node;
-for (i = 0; info->argv[i]; i++)
-{
-if (info->argv[i][0] != '$' || !info->argv[i][1])
-continue;
-if (!_strcmp(info->argv[i], "$?"))
-{
-replace_string(&(info->argv[i]),
-_strdup(convert_number(info->status, 10, 0)));
-continue;
-}
-if (!_strcmp(info->argv[i], "$$"))
-{
-replace_string(&(info->argv[i]),
-_strdup(convert_number(getpid(), 10, 0)));
-continue;
-}
-node = node_starts_with(info->env, &info->argv[i][1], '=');
-if (node)
-{
-replace_string(&(info->argv[i]),
-_strdup(_strchr(node->str, '=') + 1));
-continue;
-}
-replace_string(&info->argv[i], _strdup(""));
-}
-return (0);
-}
-/**
-* replace_string - replaces string
-* @old: address of old string
-* @new: new string
-*
-* Return: 1 if replaced, 0 otherwise
-*/
-int replace_string(char **old, char *new)
-{
-free(*old);
-*old = new;
-return (1);
 }
 
